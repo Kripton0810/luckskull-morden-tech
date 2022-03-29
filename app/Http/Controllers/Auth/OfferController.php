7d9894d7326a721fd03fefbf076b789aec8ff92b
Kripton0810 <subhankar0810@gmail.com>
2022-03-29 18:52:19 +0530

    subhankar

---
+++ app/Http/Controllers/Auth/OfferController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules;
+
+class OfferController extends Controller
+{
+    public function index()
+    {
+        return view('auth.offers');
+    }
+}
